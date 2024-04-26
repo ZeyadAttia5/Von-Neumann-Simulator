0531f16c816a66e914f61939d8c7e9c69cc7b623
mowaell <mwael0304@gmail.com>
2024-04-26 15:41:10 +0200

    Added the jeq and the and instructions implementations

--- src/instruction_set.c
+++ src/instruction_set.c
@@ -11,3 +11,15 @@ void movi(Instruction *instruction)
 {
     instruction->result = instruction->immediate;
 }
+
+void jeq (Instruction * instruction)
+{
+    if((read_register(instruction->r1) - read_register(instruction->r2))==0){
+    instruction->result = read_register(32)+4*(1+instruction->immediate);
+    }
+}
+
+void and (Instruction * instruction)
+{
+    instruction->result = read_register(instruction->r2) & read_register(instruction->r3);
+}
